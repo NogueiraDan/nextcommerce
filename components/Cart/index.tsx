7f2d582bebc04fd0376f6fce08783ba54a8952b7
NogueiraDan <silvanogueira3460@gmail.com>
2023-09-06 14:43:26 -0300

    Esqueleto do carrinho implementado

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -1,22 +1,50 @@
 import React from "react";
 import styles from "./cart.module.css";
+import { useCart } from "@/context/cartContext";
 
 const Cart = ({ open, setOpen }: any) => {
+  const { cart, removeFromCart } = useCart();
+
+  const handleRemoveItem = (itemId: any) => {
+    removeFromCart(itemId);
+  };
+
   return (
     <div className={styles.cart}>
       <div className={styles.cartHeader}>
         <h1>Carrinho</h1>
         <span onClick={() => setOpen(!open)}>Fechar</span>
       </div>
-      <div className={styles.cartBody}>
-        <p>Item</p>
-      </div>
-      <div>
-        <span>SUBTOTAL</span>
-        <span>$100</span>
-      </div>
-      <button>PROCEED TO CHECKOUT</button>
-      <span>Reset Cart</span>
+      {cart.length>0 && (
+        <>
+          <div className={styles.cartBody}>
+            {cart.map((item) => {
+              return (
+                <>
+                  <p>{item.title}</p>
+                  <p>{item.quantity} unidades</p>
+                  <button onClick={() => handleRemoveItem(item.id)}>
+                    Remove
+                  </button>
+                </>
+              );
+            })}
+          </div>
+          <div>
+            <span>SUBTOTAL</span>
+            <span>$100</span>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+          <span>Reset Cart</span>
+        </>
+      )}
+      {
+        cart.length===0 && (
+          <>
+            <h2>Seu carrinho tá vazio!</h2>
+          </>
+        )
+      }
     </div>
   );
 };
